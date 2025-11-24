0494cbd1f98a7202eef9c07dcea5ef9b8dbdf116
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-11-24 15:49:29 +0100

    push
    push

---
+++ Progetti-SW2/LINQ/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numeri = {1,2,3,6,2,4,5,7,8};
+var numeriPari = from numero in numeri
+                 where numero % 2 == 1
+                 select numero;
+foreach (var n in numeriPari)
+{
+    Console.WriteLine(n);
+}
