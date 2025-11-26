b7ad18d4624a3c6bf059de9ab760f24a1578c53d
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-11-26 22:23:29 +0100

    a
    a

--- Progetti-SW2/CodeWars_Exercises/sum_nth_series/Program.cs
+++ Progetti-SW2/CodeWars_Exercises/sum_nth_series/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class NthSeries {
+	
+	public static string SeriesSum (int n) {
+        double somma = 0.00f;
+        for (int m=0; m < n; m++){
+            somma += n*(1 + 1/(1+3*m));
+
+            if(m==n-1){
+                return String.Format("{0:0.00}",somma);
+            }
+        }
+	}
+}
