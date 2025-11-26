da7b6f7e615b75c0bc63e6422b057a1a1758f685
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-11-26 22:10:00 +0100

    aa
    aaa

--- Progetti-SW2/CodeWars_Exercises/Kategorize/Program.cs
+++ Progetti-SW2/CodeWars_Exercises/Kategorize/Program.cs
@@ -7,10 +7,14 @@ public class Kata
 {
     public static IEnumerable<string> OpenOrSenior(int[][] data)
     {
-        foreach(var a in data[a][]&& var h in data[][h])
-            if (a > 54 && h >= 7)
+        foreach (var item in data)
+        {
+            int age = item[0];
+            int handicap = item[1];
+            if (age > 54 && handicap > 7)
                 yield return "Senior";
             else
                 yield return "Open";
+        }
     }
 }
