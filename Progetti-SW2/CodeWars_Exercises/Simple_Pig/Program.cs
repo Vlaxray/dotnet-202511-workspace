e5343fab8c7c142f33fb8af94c6df1b5d03e2a09
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-11-26 22:57:09 +0100

    qq
    qq

--- Progetti-SW2/CodeWars_Exercises/Simple_Pig/Program.cs
+++ Progetti-SW2/CodeWars_Exercises/Simple_Pig/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string PigIt(string str)
+    {
+        string[] words = str.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            string w = words[i];
+
+            // Se è punteggiatura, la lasciamo com'è
+            if (w.Length == 1 && !char.IsLetter(w[0]))
+                continue;
+
+            // Pig Latin: sposta prima lettera in fondo + "ay"
+            words[i] = w.Substring(1) + w[0] + "ay";
+        }
+        return string.Join(" ", words);
+    }
+}
