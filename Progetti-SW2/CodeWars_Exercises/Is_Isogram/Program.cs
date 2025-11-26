517312c6b5a286a193976eb1f6884a5542836f7f
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-11-26 18:22:38 +0100

    pushing the push

---
+++ Progetti-SW2/CodeWars_Exercises/Is_Isogram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static bool IsIsogram(string str)
+    {
+        str = str.ToLower();
+        HashSet<char> seen = new HashSet<char>();
+
+        foreach (char c in str)
+        {
+            if (seen.Contains(c))
+                return false; // ripetizione trovata
+            seen.Add(c);
+        }
+
+        return true; // nessuna ripetizione
+    }
+}
