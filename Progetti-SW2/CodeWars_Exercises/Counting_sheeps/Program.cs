09044e432837842e4f7d6b60d5a7fc088fc93a25
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-11-26 19:47:00 +0100

    pushing the sheep
    pushing the sheep

---
+++ Progetti-SW2/CodeWars_Exercises/Counting_sheeps/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int CountSheeps(bool[] sheeps)
+    {
+        int sommaPecore = 0;
+        int sommaPecoreScappateDiCasa = 0;
+        foreach (var s in sheeps)
+        {
+            if (s == true)
+                sommaPecore += 1;
+        }
+
+        foreach (var s in sheeps)
+        {
+            if (s == false)
+            {
+                sommaPecoreScappateDiCasa += 1;
+            }
+        }
+        Console.WriteLine("Il numero di pecore è: " + sommaPecore);
+        Console.WriteLine("Il numero di pecore scappate da casa è: " + sommaPecoreScappateDiCasa);
+        return sommaPecore;
+
+
+
+    }
+}
