bdfb2c6f2267b5e18e05d4fd4550d6ff9ee7f79b
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-11-26 19:08:46 +0100

    push
    pus h

---
+++ Progetti-SW2/CodeWars_Exercises/You_square/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Kata
+{
+    public static bool IsSquare(int n)
+    {
+        double risultato = Math.Sqrt(n);
+        if (n < 0)
+        {
+            Console.WriteLine("Negative numbers cannot be square");
+            return false;
+        }
+        else if (risultato % 1 == 0)
+        {
+            Console.WriteLine("The number is square");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("The number isn't square");
+            return false;
+        }
+    }
+}
