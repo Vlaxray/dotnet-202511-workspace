e634b4f1c5d3466fb653677b3e318163734959f8
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-11-27 19:43:17 +0100

    aaa
    aaa

---
+++ Progetti-SW2/CodeWars_Exercises/Sum_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Sum
+{
+    public int GetSum(int a, int b)
+    {
+        int somma1 = 0;
+        int somma2 = 0;
+
+        if (a < b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                somma1 += i;
+            }
+            return somma1;
+        }
+        else if (a == b)
+        {
+            return a;
+        }
+        else
+        {
+            for (int i = b; i <= a; i++)
+            {
+                somma2 += i;
+            }
+            return somma2;
+        }
+    }
+}
---
+++ Progetti-SW2/CodeWars_Exercises/double_char/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+public class Kata
+{
+    public static string DoubleChar(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (char c in s)
+        {
+            sb.Append(c);
+            sb.Append(c); // aggiunge due volte lo stesso carattere
+        }
+        return sb.ToString();
+    }
+}
